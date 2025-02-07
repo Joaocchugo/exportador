f64274a91727d081a18c6c7def13028ffc1f14c4
Joao <146850753+Joaocchugo@users.noreply.github.com>
2025-02-07 16:25:17 -0300

    Atualização na main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,21 @@ import org.example.exportador.Produto;
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var exportador = ExportadorListaProduto.newInstance("html");
+
         var produtos = List.of(
                 new Produto(1, "Notebook", "Acer Aspire A315-N2", 3500),
                 new Produto(2, "Iphone 15 Pro Max", "Apple", 5200),
                 new Produto(3, "Monitor 60 Polegadas", "LG", 4500)
         );
-        System.out.println(exportador.exportar(produtos));
+        var exportadorPadrao = ExportadorListaProduto.newInstance();
+        System.out.println("Lista de Produtos em HTML\n");
+        System.out.println(exportadorPadrao.exportar(produtos));
+
+        var exportadorMark = ExportadorListaProduto.newInstance("md");
+        System.out.println("Lista de Produtos em MARKDOWN\n");
+        System.out.println(exportadorMark.exportar(produtos));
+
     }
 }
